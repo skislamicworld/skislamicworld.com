7b895017fce575c0b4fdb303c70997a567f84f5e
skislamicworld <157791832+skislamicworld@users.noreply.github.com>
2024-01-26 00:39:25 -0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,62 @@
+let questions = [
+
+   
+    {
+        numb:1,
+        question: "What is the most populous Muslim country in the world?",
+        answer: "b.Indonesia",
+        options: [
+            "a.Saudi Arabia",
+            "b.Indonesia",
+            "c.Pakistan",
+            "d.Iraq",
+        ]
+    },
+    {
+        numb:2,
+        question: "Which Day Are Lucky For Muslim ?",
+        answer: "C. Friday",
+        options: [
+            "A. Saturday",
+            "B. Monday",
+            "C. Friday",
+            "D. Sunday",
+        ]
+    },
+    {
+        numb:3,
+        question: "In Islam, the supreme central focus and authority is ?",
+
+        answer: "a.Allah",
+        options: [
+            "a.Allah",
+            "b.Caliph",
+            "c.Muhammad",
+            "d.Imam",
+        ]
+    },
+    {
+        numb:4,
+        question: "The word “Islam” means ?",
+        answer: "b.Surrender to God",
+        options: [
+            "a.Those who follow Muhammad",
+            "b.Surrender to God",
+            "c.Recitations",
+            "d.Sons of Allah",
+        ]
+    },
+    {
+        numb:5,
+        question: "What is the Shari’ah?",
+        answer: "d.A form of Muslim law",
+        options: [
+            "a.A religious school",
+            "b.A native dance",
+            "c.A style of calligraphy",
+            "d.A form of Muslim law",
+        ]
+    }
+
+
+];
